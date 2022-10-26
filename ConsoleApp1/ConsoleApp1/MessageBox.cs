1296cb617ce2166c5f0f74d3047472a5ec8263f4
Vladyslav Drushliak <vladislav.drushliak@gmail.com>
2022-10-26 16:13:05 +0300

    Done

---
+++ ConsoleApp1/ConsoleApp1/MessageBox.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class MessageBox
+    {
+        public event Action<State> MyEvent;
+        public State GetRandState
+        {
+            get
+            {
+                switch (new Random().Next(1, 3))
+                {
+                    case 1:
+                        return State.Cancel;
+                    case 2:
+                        return State.Ok;
+                    default:
+                        return State.Error;
+                }
+            }
+        }
+
+        public async void Open()
+        {
+            Console.WriteLine("The window is open");
+            await Task.Delay(3000);
+            Console.WriteLine("The window is closed");
+            MyEvent.Invoke(GetRandState);
+        }
+    }
+}
--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,11 +1,33 @@
 ﻿using System;
+using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
+            var mBox = new MessageBox();
+
+            mBox.MyEvent += state =>
+            {
+                if (state == State.Cancel)
+                {
+                    Console.WriteLine("операція була відхилена");
+                }
+
+                if (state == State.Ok)
+                {
+                    Console.WriteLine("операція була підтверджена");
+                }
+            };
+
+            var tcs = new TaskCompletionSource();
+
+            mBox.MyEvent += x => tcs.SetResult();
+            mBox.Open();
+
+            await tcs.Task;
         }
     }
 }
